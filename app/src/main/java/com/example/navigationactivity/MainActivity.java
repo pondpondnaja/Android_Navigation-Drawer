d5b464fe1a05d8874e312d4e3a03d765f8306daf
P_Petch <futayo110@gmail.com>
2019-07-30 21:28:55 +0700

    Clean up

--- app/src/main/java/com/example/navigationactivity/MainActivity.java
+++ app/src/main/java/com/example/navigationactivity/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
+
         return true;
     }
 
